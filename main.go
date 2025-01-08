a805760efcfd3f53f726aff6b2c827fa8c5d5365
Nitish Sharma <sharnit2825@gmail.com>
2025-01-08 15:00:46 +0000

    added read from aof on start

--- main.go
+++ main.go
@@ -23,6 +23,20 @@ func main() {
 	}
 	defer aof.Close()
 
+	// TODO: Fix read
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			return
+		}
+
+		handler(args)
+	})
+
 	// Listen for connections
 	conn, err := l.Accept()
 	if err != nil {
