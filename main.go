647639cf3c12b85ddcd153760b96d8b9b348faa0
Nitish Sharma <sharnit2825@gmail.com>
2025-01-08 14:46:14 +0000

    added persistence using aof

--- main.go
+++ main.go
@@ -16,13 +16,19 @@ func main() {
 		return
 	}
 
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
 	// Listen for connections
 	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-
 	defer conn.Close()
 
 	for {
@@ -53,6 +59,10 @@ func main() {
 			fmt.Println("Invalid command: ", command)
 			writer.Write(Value{typ: "string", str: ""})
 			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
 		}
 
 		result := handler(args)
