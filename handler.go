86f64fb19a25a49473eddfd01e7ba8a6743c63f6
Nitish Sharma <sharnit2825@gmail.com>
2025-01-08 14:29:38 +0000

    added hget/hset commands

--- handler.go
+++ handler.go
@@ -6,10 +6,15 @@ var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
 	"SET":  set,
 	"GET":  get,
+	"HSET": hset,
+	"HGET": hget,
 }
 
 var SETs = map[string]string{}
 var SETsMu = sync.RWMutex{}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -44,6 +49,44 @@ func get(args []Value) Value {
 	SETsMu.RLock()
 	value, ok := SETs[key]
 	SETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+	HSETs[hash][key] = value
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
 
 	if !ok {
 		return Value{typ: "null"}
