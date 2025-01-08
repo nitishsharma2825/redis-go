73241ca162d7536001ecfb8f1d70c45756983bf0
Nitish Sharma <sharnit2825@gmail.com>
2025-01-08 14:20:25 +0000

    added get/set command

--- handler.go
+++ handler.go
@@ -1,8 +1,15 @@
 package main
 
+import "sync"
+
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
+	"SET":  set,
+	"GET":  get,
 }
+
+var SETs = map[string]string{}
+var SETsMu = sync.RWMutex{}
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -10,4 +17,37 @@ func ping(args []Value) Value {
 	}
 
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].bulk
+	value := args[1].bulk
+
+	SETsMu.Lock()
+	SETs[key] = value
+	SETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'get' command"}
+	}
+
+	key := args[0].bulk
+
+	SETsMu.RLock()
+	value, ok := SETs[key]
+	SETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
 }
